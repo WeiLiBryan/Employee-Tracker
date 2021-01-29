c399fdfed18a96b1779956d3e3aaac719b6e21da
William Bryan <wwbryan00@gmail.com>
2021-01-29 02:27:53 -0800

    add switch navigation for roles, departments and employees

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,4 +17,48 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
+  init();
 });
+
+function init() {
+    inquirer.prompt({
+        name: "nav",
+        type: "list",
+        message: "What would you like to view/edit?",
+        choices: [
+            "Departments",
+            "Roles",
+            "Employees"
+        ]
+    }).then(res => {
+        switch(res.nav){
+            case "Departments":
+                departmentNav();
+                break;
+
+            case "Roles":
+                roleNav();
+                break;
+
+            case "Employees":
+                empNav();
+                break;
+        }
+    });
+}
+
+
+
+// "View All Departments",
+// "Add Departments",
+// "Remove Departments",
+// "View Department Budget",
+// "View All Roles",
+// "Add New Role",
+// "Remove Role",
+// "View All Employees"
+// "Add New Employee",
+// "Remove Employee",
+// "View Employee by Department",
+// "View Employee by Manager",
+// "Update Employee Manager",
